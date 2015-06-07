d4cc804cb8b2235501d086eab1b01cf07b8a31ec
Anna Taberski <aftaberski@gmail.com>
2015-06-07 15:03:08 -0400

    Remove autopublish and set up publication

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,18 +1,5 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  }
-];
-// Tells postsList template to use postsData as posts
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
